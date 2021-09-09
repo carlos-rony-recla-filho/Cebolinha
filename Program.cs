3838821179ce9682f69c351daa276af1db897e00
Carlos Rony <carlosrecla03@gmail.com>
2021-09-09 18:39:04 -0300

    final da tarefa.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FraseDigitada, fraseComDislalia;
+            
+            Console.Write(" Você dizendo:"); 
+            FraseDigitada = Console.ReadLine();
+            fraseComDislalia = FraseDigitada.Replace("R", "L").Replace("r", "l");
+
+            Console.WriteLine();
+            
+            Console.Write(" O Cebolinha Dizendo:");
+            Console.WriteLine(fraseComDislalia);
+            
+        }
+    }
+}
